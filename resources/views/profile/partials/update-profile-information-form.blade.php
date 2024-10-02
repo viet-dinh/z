e1c61daf790d7424e25b1f02ee0f82aef9b5bc7c
viet dinh <dinhquocvietpro@gmail.com>
2024-10-03 00:39:50 +0700

    chore(profile): title profile

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Thông tin trang cá nhân') }}
+            {{ __('Thông tin') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
