3f1af5606209133cf52d1284814e9d6ad975892b
viet dinh <viet.dinh@frogasia.com>
2024-10-07 17:35:21 +0700

    feat(i18n): mail locally and add new translations

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,22 @@ return [
     |
     */
 
+    'login' => 'Login',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'forgot_password' => 'Forgot Password',
+    'register_account' => 'Register Account',
+    'register' => 'Register',
+    'fullname' => 'Full Name',
+    'login_with' => 'Login with :name',
+    'forgot_password_description' => 'Forgot your password? No problem. Just let us know your email address and we will send you a password reset link that allows you to choose a new password.',
+    'forgot_password_button' => 'Send Link to Email',
+    'password_confirmation' => 'Confirm Password',
+    'already_registered' => 'Go back to the login page',
+
     'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'incorrect_password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
+    'reset_password_confirm' => 'Confirm'
 ];
--- lang/vi/auth.php
+++ lang/vi/auth.php
@@ -12,5 +12,11 @@ return [
     'forgot_password_description' => 'Quên mật khẩu của bạn? Không sao. Hãy cho chúng tôi biết địa chỉ email của bạn và chúng tôi sẽ gửi cho bạn một link đặt lại mật khẩu, cho phép bạn chọn mật khẩu mới.',
     'forgot_password_button' => 'Gửi link đến email',
     'password_confirmation' => 'Nhập lại mật khẩu',
-    'already_registered' => 'Quây về trang đăng nhập'
+    'already_registered' => 'Quây về trang đăng nhập',
+
+    'failed' => 'Thông tin đăng nhập này không khớp với hồ sơ của chúng tôi.',
+    'incorrect_password' => 'Mật khẩu được cung cấp không chính xác.',
+    'throttle' => 'Quá nhiều lần đăng nhập thất bại. Vui lòng thử lại sau :seconds giây.',
+
+    'reset_password_confirm' => 'Xác nhận'
 ];
